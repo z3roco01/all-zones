12086d9b54c23530a8088b0d274e5ccc475c2e02
zerocool <46464404+z3roco01@users.noreply.github.com>
2024-11-04 19:08:18 -0600

    init

---
+++ z3roco01.allZones/MainMapMod.cs
@@ -0,0 +1,28 @@
+﻿using GDWeave;
+using GDWeave.Godot;
+using GDWeave.Godot.Variants;
+using GDWeave.Modding;
+
+namespace z3roco01.allZones;
+
+public class MainMapMod : IScriptMod{
+    public bool ShouldRun(string path) => path == "res://Scenes/Map/main_map.gdc";
+
+    public IEnumerable<Token> Modify(string path, IEnumerable<Token> tokens) {
+        var waiter = new MultiTokenWaiter([
+            t => t is IdentifierToken{Name:"child"},
+            t => t.Type is TokenType.Period,
+            t => t is IdentifierToken{Name:"visible"},
+            t => t.Type is TokenType.OpAssign,
+        ], allowPartialMatch: false);
+
+        foreach(var token in tokens) {
+            yield return token;
+
+            if (waiter.Check(token)) {
+                yield return new ConstantToken(new BoolVariant(true));
+                yield return new Token(TokenType.Newline, 2);
+            }
+        }
+    }
+}
---
+++ z3roco01.allZones/Mod.cs
@@ -0,0 +1,16 @@
+﻿using GDWeave;
+
+namespace z3roco01.allZones;
+
+public class Mod : IMod {
+    public static IModInterface ModInterface;
+
+    public Mod(IModInterface modInterface) {
+        ModInterface = modInterface;
+        modInterface.RegisterScriptMod(new MainMapMod());
+        modInterface.Logger.Information("now you have all the zones !");
+    }
+
+    public void Dispose() {
+    }
+}
